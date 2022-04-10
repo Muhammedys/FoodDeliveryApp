013969848884ccb43728a08ba771aba328f60929
Muhammedys <64150021+Muhammedys@users.noreply.github.com>
2022-04-11 02:43:26 +0300

    11-04

---
+++ lib/home/food_page_body.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class FoodPageBody extends StatefulWidget {
+  const FoodPageBody({Key? key}) : super(key: key);
+
+  @override
+  State<FoodPageBody> createState() => _FoodPageBodyState();
+}
+
+class _FoodPageBodyState extends State<FoodPageBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 320,
+      child: PageView.builder(
+          itemCount: 5,
+          itemBuilder: ((context, Position) {
+            return _buildPageItem(Position);
+          })),
+    );
+  }
+
+  Widget _buildPageItem(int index) {
+    return Container(
+      height: 220,
+      margin: EdgeInsets.only(left: 5, right: 5),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          color: index.isEven ? Color(0xff69c5df) : Color(0xff9294cc),
+          image: DecorationImage(
+              image: AssetImage("assets/image/food1.png"),
+              fit: BoxFit.contain)),
+    );
+  }
+}
---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/home/food_page_body.dart';
+import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/widgets/big_text.dart';
+import 'package:food_delivery/widgets/small_text.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      children: [
+        Container(
+          child: Container(
+            margin: EdgeInsets.only(top: 45, bottom: 15),
+            padding: EdgeInsets.only(left: 20, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    BigText(
+                      text: 'Tanzania',
+                      color: AppColors.mainColor,
+                    ),
+                    Row(children: [
+                      SmallText(text: 'Dar es Salaam'),
+                      Icon(Icons.arrow_drop_down_rounded)
+                    ]),
+                  ],
+                ),
+                Center(
+                  child: Container(
+                    width: 45,
+                    height: 45,
+                    child: Icon(
+                      Icons.search,
+                      color: Colors.white,
+                    ),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: AppColors.mainColor),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+        FoodPageBody()
+      ],
+    ));
+  }
+}
